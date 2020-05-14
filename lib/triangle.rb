f6ac235a795a6535a6003c80cb21ec0d6c1c2abd
Sonja Parsell <hello@sparsell.com>
2020-05-14 20:33:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+
+  def initialize(one, two, three)
+
+  end
+
+  def kind #returns, as a symbol, its type. The valid types are :equilateral :isosceles :scalene
+
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
