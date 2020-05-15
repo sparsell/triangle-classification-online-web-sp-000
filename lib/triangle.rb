8e0de1b040f4c716311aa3dc59f3cb2d8c3cc9b7
Sonja Parsell <hello@sparsell.com>
2020-05-15 10:38:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,7 +28,7 @@ class Triangle
       (@side_1 == @side_2) && (@side_2 == @side_3)
       :equilateral
     elsif
-      (@side_1 == @side_2) || (@side_3 < @side_1)
+      (@side_1 == @side_2) || (@side_3 == @side_1)
       :isosceles
     elsif
       (@side_1 != @side_2) && (@side_2 != @side_3)
