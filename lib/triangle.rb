6d7892957a7e45ca3666aceba779ded4c9d900c2
Sonja Parsell <hello@sparsell.com>
2020-05-15 10:01:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
       (side_1 == side_2) && (side_3 < side_1)
       :isosceles
     elsif
-      (side_1 !== side_2) && (side_2 !== side_3)
+      (side_1 != side_2) && (side_2 != side_3)
       :scalene
     else
   #   begin
