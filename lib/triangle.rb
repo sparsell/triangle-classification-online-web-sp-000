cf1255841451f615b9d5e439b4d26f5d9183c844
Sonja Parsell <hello@sparsell.com>
2020-05-15 10:34:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,20 @@ class Triangle
   end
 
 #returns, as a symbol, its type. The valid types are :equilateral :isosceles :scalene
-#scalene - has no equal sides: side_1 != side_2 && side_1 !== side_3 && side_2 !== side_3
+#scalene - has no equal sides
 #isosceles - has two equal sides
 #equilateral - three equal sides
 
 #should raise a custom error, TriangleError if the triangle is invalid
+#The sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+  def valid
+    if si
+      if (@side_1 + @side_2 + @side_3) != 180 #triangles violating triangle inequality are illegal
+        
+  end
+
   def kind
-    if (@side_1 + @side_2 + @side_3) != 180 #triangles violating triangle inequality are illegal
+    if !valid?
       begin
         raise TriangleError
       rescue TriangleError => error
