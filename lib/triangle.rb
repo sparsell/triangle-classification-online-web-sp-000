84b94bfa85070e680306ff1e097fab03a206b79f
Sonja Parsell <hello@sparsell.com>
2020-05-15 10:48:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,44 @@
+#sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+
 class Triangle
 
-  def initialize(one, two, three)
-
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
-  def kind #returns, as a symbol, its type. The valid types are :equilateral :isosceles :scalene
+#The sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+  def valid?
+    sides = [@side_1, @side_2, @side_3]
+    value_test = sides.all? {|side| side >0}
+    value_test && (@side_1 + @side_2 > @side_3) && (@side_2 + @side_3 > @side_1) && (@side_1 + @side_3 > @side_2)
+  end
 
+  def kind
+    if !valid?
+      raise TriangleError
+    elsif
+      (@side_1 == @side_2) && (@side_2 == @side_3)
+      :equilateral
+    elsif
+      (@side_1 == @side_2) || (@side_3 == @side_1) || (@side_2 == @side_3)
+      :isosceles
+    elsif
+      (@side_1 != @side_2) && (@side_2 != @side_3)
+      :scalene
+    else
+    end
   end
 
   class TriangleError < StandardError
+    def message
 
+
+
+    #   if
+    # end
+
+    end
   end
 end
