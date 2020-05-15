be3234aba0ab7fb0e4f34dee3f38a8e0a7dfac8e
Sonja Parsell <hello@sparsell.com>
2020-05-15 10:47:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,8 @@ class Triangle
 #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side
   def valid?
     sides = [@side_1, @side_2, @side_3]
-    sides.all? {|side| side >0}
-
-    #(@side_1 + @side_2 > @side_3) || (@side_1 + @side_3 > @side_2)
+    value_test = sides.all? {|side| side >0}
+    (@side_1 + @side_2 > @side_3) || (@side_1 + @side_3 > @side_2)
   end
 
   def kind
