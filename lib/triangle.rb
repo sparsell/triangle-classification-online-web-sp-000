0b70b551359f63d42ba368d3a4ddbeba220eb71c
Sonja Parsell <hello@sparsell.com>
2020-05-15 09:19:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,19 @@
+#sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+
 class Triangle
 
   def initialize(one, two, three)
 
   end
 
-  def kind #returns, as a symbol, its type. The valid types are :equilateral :isosceles :scalene
+#should raise a custom error, TriangleError if the triangle is invalid
+#returns, as a symbol, its type. The valid types are :equilateral :isosceles :scalene
+  def kind 
+    #if one + two + three != 180
+    begin
+    raise error
+  rescue
+  end
 
   end
 
